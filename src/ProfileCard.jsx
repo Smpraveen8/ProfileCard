9de2c09d18ef8496c88ac2d219e6c71e6b04e109
Smpraveen8 <smpraveen2002@gmail.com>
2024-11-12 16:02:27 +0530

    add:resume file

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 
 function res() {
-  window.open('https://raw.githubusercontent.com/Smpraveen8/ProfileCard/main/public/PraveenSM.pdf', '_blank');
+  
+  window.open('https://smpraveen8.github.io/ProfileCard/PraveenSM.pdf', '_blank');
 }
 
 function User() {
@@ -12,22 +13,26 @@ function User() {
       <a className='pro' href='https://www.linkedin.com/in/praveen-sm-742209212/' target="_blank" rel="noopener noreferrer">Linkedin</a>
       <a className='pro2' href='https://github.com/Smpraveen8' target="_blank" rel="noopener noreferrer">Github</a>
 
-      {/* Profile Image */}
-      <img src='/photo1.jpg' className='image' width="100" height="100" alt="Profile" />
+     
+      <img 
+        src='https://raw.githubusercontent.com/Smpraveen8/ProfileCard/main/public/photo1.jpg' 
+        className='image' 
+        width="100" 
+        height="100" 
+        alt="Profile" 
+      />
 
-      {/* Name and Info */}
       <h3 className='name'>Praveen SM</h3>
       <h3 className='dept'>smpraveen2002@gmail.com</h3>
       <h3 className='dept'>Electronics and Communication Engineering</h3>
       <p className='dept'>Full Stack Developer</p>
 
-      {/* Resume Button */}
+      
       <div className='button'>
-        {/* Button to download/view resume */}
         <button className='primary outline' onClick={res}>Resume</button>
       </div>
 
-      {/* Skills Section */}
+     
       <div className='skills'>
         <h6 className='sk'>Skills</h6>
         <ul>
