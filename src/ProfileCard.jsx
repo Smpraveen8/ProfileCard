83196c32cefcd21c328e7beb6b3a9eb56649414f
Smpraveen8 <smpraveen2002@gmail.com>
2024-11-12 16:11:57 +0530

    resume2

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,38 +1,50 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 function res() {
-  
-  window.open('https://smpraveen8.github.io/ProfileCard/PraveenSM.pdf', '_blank');
+  // Update this to toggle the PDF display
+  setShowResume(true);
 }
 
 function User() {
+  const [showResume, setShowResume] = useState(false);
+
   return (
     <div className='card-container'>
       {/* Social Links */}
       <a className='pro' href='https://www.linkedin.com/in/praveen-sm-742209212/' target="_blank" rel="noopener noreferrer">Linkedin</a>
       <a className='pro2' href='https://github.com/Smpraveen8' target="_blank" rel="noopener noreferrer">Github</a>
 
-     
+      {/* Profile Image */}
       <img 
-        src='https://raw.githubusercontent.com/Smpraveen8/ProfileCard/main/public/photo1.jpg' 
+        src='photo1.jpg'
         className='image' 
         width="100" 
         height="100" 
         alt="Profile" 
       />
 
+      {/* Profile Name and Info */}
       <h3 className='name'>Praveen SM</h3>
       <h3 className='dept'>smpraveen2002@gmail.com</h3>
       <h3 className='dept'>Electronics and Communication Engineering</h3>
       <p className='dept'>Full Stack Developer</p>
 
-      
+      {/* Resume Button */}
       <div className='button'>
         <button className='primary outline' onClick={res}>Resume</button>
       </div>
 
-     
+      {/* Show PDF inside an iframe if the user clicks 'Resume' */}
+      {showResume && (
+        <iframe 
+          src="https://smpraveen8.github.io/ProfileCard/PraveenSM.pdf" 
+          width="100%" 
+          height="600px" 
+          title="Resume"
+        />
+      )}
+
+      {/* Skills Section */}
       <div className='skills'>
         <h6 className='sk'>Skills</h6>
         <ul>
