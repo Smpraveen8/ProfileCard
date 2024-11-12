4bbf4057573e5b3886b864b58b5a87bbb65d2795
Smpraveen8 <smpraveen2002@gmail.com>
2024-11-12 15:52:23 +0530

    adding:resume12/11

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,41 +1,52 @@
+import React from 'react';
 
-import React from 'react'
-function res(){
-  window.location.href = process.env.PUBLIC_URL + '/PraveenSM.pdf'; 
+
+function res() {
+  window.open('https://raw.githubusercontent.com/Smpraveen8/ProfileCard/main/public/PraveenSM.pdf', '_blank');
 }
-function User(){
-  return(
+
+function User() {
+  return (
     <div className='card-container'>
-    <a className='pro' href='https://www.linkedin.com/in/praveen-sm-742209212/'>Linkedin</a>
-    <a className='pro2' href='https://github.com/Smpraveen8'>Github</a>
-    <img src='photo1.jpg' className='image' width= "100" height="100"></img>
-    <h3 className='name'>Praveen SM</h3>
-    <h3 className='dept'>smpraveen2002@gmail.com</h3>
-    <h3 className='dept'>Electronics and Communication Engineering</h3>
-    <p className='dept'>Full Stack Developer</p>
-    <div className='button'>
-      {/* <button className='primary'>Message</button> */}
-      <button className='primary outline' onClick={res}>Resume</button>
+      {/* Social Links */}
+      <a className='pro' href='https://www.linkedin.com/in/praveen-sm-742209212/' target="_blank" rel="noopener noreferrer">Linkedin</a>
+      <a className='pro2' href='https://github.com/Smpraveen8' target="_blank" rel="noopener noreferrer">Github</a>
+
+      {/* Profile Image */}
+      <img src='/photo1.jpg' className='image' width="100" height="100" alt="Profile" />
+
+      {/* Name and Info */}
+      <h3 className='name'>Praveen SM</h3>
+      <h3 className='dept'>smpraveen2002@gmail.com</h3>
+      <h3 className='dept'>Electronics and Communication Engineering</h3>
+      <p className='dept'>Full Stack Developer</p>
+
+      {/* Resume Button */}
+      <div className='button'>
+        {/* Button to download/view resume */}
+        <button className='primary outline' onClick={res}>Resume</button>
+      </div>
+
+      {/* Skills Section */}
+      <div className='skills'>
+        <h6 className='sk'>Skills</h6>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React JS</li>
+          <li>Java</li>
+          <li>Python</li>
+          <li>MySQL</li>
+          <li>Oracle</li>
+        </ul>
+      </div>
     </div>
-    <div className='skills'>
-      <h6 className='sk'>Skills</h6>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>Javascript</li>
-        <li>React JS</li>
-         <li>Java</li>
-         <li>Python</li>
-         <li>Mysql</li>
-         <li>Oracle</li>
-      </ul>
-    </div>
-    </div>
-  )
-      
+  );
 }
+
 export const ProfileCard = () => {
   return (
-    < User />
-  )
+    <User />
+  );
 }
