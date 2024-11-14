17bedc46aa7219cf86c38999172d9e06f23f1f95
Smpraveen8 <smpraveen2002@gmail.com>
2024-11-14 15:00:50 +0530

    changes new

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -8,18 +8,18 @@ function res(){
 function User(){
   return(
     <div className='card-container'>
-    <a className='pro' href='https://www.linkedin.com/in/praveen-sm-742209212/'>Linkedin</a>
-    <a className='pro2' href='https://github.com/Smpraveen8'>Github</a>
+    <a className='pro' href='https://www.linkedin.com/in/praveen-sm-742209212/'>🔗 Linkedin</a>
+    <a className='pro2' href='https://github.com/Smpraveen8'>🔗 Github</a>
     <img src='photo4.jpg' className='image' width= "120" height="120"></img>
     <h3 className='name'>Praveen SM</h3>
-    <h3 className='dept'>smpraveen2002@gmail.com</h3>
-    <h3 className='dept'>Electronics and Communication Engineering</h3>
-    <p className='dept'>Full Stack Developer</p>
+    <h3 className='dept'>📩 smpraveen2002@gmail.com</h3>
+    <h3 className='dept'>👨🏻‍🎓 Electronics and Communication Engineering</h3>
+    <p className='dept'>👨🏻‍💻 Full Stack Developer</p>
     <div className='button'>
       <button className='primary outline' onClick={res}>Resume</button>
     </div>
     <div className='skills'>
-      <h6 className='sk'>Skills</h6>
+      <h6 className='sk'>Skills:</h6>
       <ul>
         <li>HTML</li>
         <li>CSS</li>
